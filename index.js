f2c5be126f4e41af7ebfe800d5a80e9e35a47ac2
Fran Diaz Deheza <84636613+FranDiazDeheza@users.noreply.github.com>
2021-09-20 11:40:02 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-const music = new Audio('argentinidad.mp3');
-
 const sumar = () => {
     const calform = document.getElementById('calform');
     const result = eval(calform.display.value);
-    calform.display.value = result || 0;
+    calform.display.value = result ? result : 0;
 
-    music.load();
+    const music = new Audio('coscu.mp3');
     music.play();
 }
